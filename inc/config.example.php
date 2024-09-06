09be7fcb2c733f1d1995d76ff898aaca3cfc4542
vladislav <a.vladislav.g@gmail.com>
2024-09-06 16:27:51 +0500

    образец конфигурационного файла, включающий недокументированные опции

---
+++ inc/config.example.php
@@ -0,0 +1,30 @@
+<?php
+$dbhostname = "127.0.0.1";
+
+$dbusername = "salesman";
+$dbpassword = "salesman!1";
+$database   = "salesman";
+$sqlname    = "app_";
+
+//закрываем на обслуживание
+//$istimeout = true;
+
+//ограничение по диску в Мб
+$diskLimit = 10000;
+//доп.пространство на аккаунт
+$diskAddon = 0;
+
+// максимальный уровень вложености папок для файлов
+$maxFileFolderLevel = 3;
+
+// максимальный уровень вложености категорий прайса
+$maxPriceFolderLevel = 3;
+
+//сообщение о переезде
+//$isremoval = true;
+//$removalURL = '';
+
+//$language = 'en-EN';
+
+//своё изображение на странице входа
+//$wallpaper = '';
